8bb09c92f32a780b61bbbcc33a60b2e00c39d481
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2023-10-12 09:25:59 +0530

    Time: 20 ms, Memory: 23.7 MB - LeetHub

--- reverse-string/reverse-string.cpp
+++ reverse-string/reverse-string.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        reverseHelper(s, 0);
-    }
-
-    void reverseHelper(vector<char>& s, int index) {
-        int n = s.size();
-
-        if (index >= n / 2) {
-            return; // Base case: Stop recursion when the pointer reaches the middle of the string.
+    
+    void reverse(vector<char> & s, int i){
+          int n = s.size();
+        
+      
+        
+        if(i>=n/2){
+            return;
+            
         }
-
-        // Swap the characters at the current index and the corresponding index from the end.
-        swap(s[index], s[n - index - 1]);
-
-        // Recursively call reverseHelper with the next index.
-        reverseHelper(s, index + 1);
+            
+            
+            swap(s[i], s[n-i-1]);
+            reverse(s, i+1);
+        
+        
+        
+    }
+    void reverseString(vector<char>& s) {
+        
+        reverse(s,0);
+     
     }
 };
