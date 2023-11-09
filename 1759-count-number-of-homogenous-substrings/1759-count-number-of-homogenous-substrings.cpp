36270be834e4d3c4994129bd7d84461bbd0aac1c
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2023-11-09 10:11:36 +0530

    Time: 18 ms (97.17%), Space: 12.2 MB (33.48%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int countHomogenous(string s) {
-        long long sum=0;
+        long long sum=1;
         long long count =1;
         const long long mod = 1000000007;
         
         for(int i=0; i<s.length()-1; i++){
             if(s[i]==s[i+1]){
                 count++;
+                sum = (sum + count)%mod;
+                
             }else{
-                sum += (count * (count+1)/2%mod)%mod;
+                
                 count =1;
+                sum = (sum + count)%mod;
                 
             }
             
         }
-        sum = (sum + count * (count+1)/2%mod)%mod;
+        
         return sum;
         
     }
